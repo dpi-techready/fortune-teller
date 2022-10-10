19b8bee8bdbf1a108cfeb6cfe4f260f2678d7fb8
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-10 17:03:11 +0000

    completed

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,15 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @unlucky_nums = Array.new
+
+    5.times do
+      nums = rand(1...100)
+
+      @unlucky_nums.push(nums)
+    end  
+    render({ :template => "lottery_stuff/ohno.html.erb"})
+  end  
 end
